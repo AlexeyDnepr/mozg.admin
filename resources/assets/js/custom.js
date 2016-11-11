5f5ed3b04c9592fe30d01ac0b28b85b09fef01b1
Alexey Ivakhnenko <mail_of_master@ukr.net>
2016-11-11 19:54:50 +0200

    Стабильная версия 11.11

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -1,89 +1,87 @@
-var h1 = document.querySelector("h1"),
-        form = document.querySelector("form.control"),
-        input_page_id_elems = document.querySelectorAll("[name^=page_id_]"),
-        check_inputs = document.querySelectorAll("[type=radio], [type=checkbox]"),
-        control_block = document.querySelector(".control_block"),
-        radio_control_blocks = control_block.querySelectorAll("input[type=radio]"),
-        btn_control_blocks = control_block.querySelectorAll(".btn"),
-        inputs_control_blocks = document.querySelectorAll(".control_block input, .control_block button");
-document.body.insertBefore(h1, document.body.firstChild);
-[].forEach.call(inputs_control_blocks, function (elem) {
+var h1 = $("h1"),
+        form = $("form.control"),
+        check_box_row = $(".check_box_row"),
+        check_inputs = $("[type=radio], [type=checkbox]"),
+        control_block = $(".control_block"),
+        radio_control_blocks = $("input[type=radio]"),
+        btn_control_blocks = $(".btn"),
+        location_pathname = location.pathname,
+        presentation,
+        inputs_control_blocks = $(".control_block input, .control_block button");
+location_pathname = location_pathname.replace(/^\/|\/$/g, '');
+location_pathname = location_pathname.split('/');
+location_pathname = location_pathname.shift();
+presentation = $("[role=presentation] [href=" + location_pathname + "]");
+presentation ? presentation.parent().addClass('active') : false;
+inputs_control_blocks.each(function (index, elem) {
     elem.disabled = true;
 });
-[].forEach.call(check_inputs, function (elem) {
+check_inputs.each(function (index, elem) {
     elem.checked = false;
 });
-form.addEventListener("change", function (e) {
-    var cur_input = e.target,
+form.on("change", function (e) {
+    var cur_input = $(e.target),
             disable_status,
             cur_input_name,
             check,
             tr_class;
-    cur_input_name = cur_input.getAttribute("name");
+    cur_input_name = cur_input.attr("name");
     if (cur_input_name.indexOf('page_id') !== -1) {
         cur_input_name = 'page_id';
+    } else if (cur_input_name.indexOf('user_id') !== -1) {
+        cur_input_name = 'user_id';
     }
+    console.log(cur_input_name);
     switch (cur_input_name) {
         case "page_id":
-            if (cur_input.checked) {
+        case "user_id":
+            if (cur_input.prop('checked')) {
                 tr_class = "marked";
                 disable_status = false;
             } else {
                 tr_class = "";
                 disable_status = true;
             }
-            cur_input.closest("tr").className = tr_class;
-            [].forEach.call(input_page_id_elems, function (elem) {
+            //console.log();
+            cur_input.closest("tr").attr('class', tr_class);
+            check_box_row.each(function (index, elem) {
                 if (elem.checked === true) {
                     disable_status = false;
                 }
             });
             check = !disable_status;
-            [].forEach.call(radio_control_blocks, function (elem) {
+            radio_control_blocks.each(function (index, elem) {
                 elem.disabled = disable_status;
             });
-
             disableSendButtons(radio_control_blocks, btn_control_blocks, check);
             break;
         case "control_block":
-            [].forEach.call(btn_control_blocks, function (elem) {
+            console.log(cur_input.attr('type'));
+            btn_control_blocks.each(function (index, elem) {
                 elem.disabled = true;
             });
-            cur_input.parentElement.querySelector(".btn").disabled = false;
+            cur_input.parent().find(".btn").prop('disabled', false);
             break;
     }
 });
+$('form.filter').on('submit', function (e) {
+    var elems = $(this).find('input, select');
+    elems.each(function (index, elem) {
+        if (!elem.value) {
+            elem.name = '';
+        }
+    });
+});
 function disableSendButtons(inputs, buttons, check) {
-    [].forEach.call(buttons, function (button) {
-        button.disabled = true;
+    buttons.each(function (index, elem) {
+        elem.disabled = true;
     });
     if (check) {
-        [].forEach.call(inputs, function (elem) {
-            if (elem.checked === true) {
-                elem.parentElement.querySelector(".btn").disabled = false;
+        inputs.each(function (index, elem) {
+            var elem = $(elem);
+            if (elem.prop('checked')) {
+                elem.parent().find(".btn").prop('disabled', false);
             }
         });
     }
 }
-(function () {
-    if (!Element.prototype.closest) {
-        Element.prototype.closest = function (css) {
-            var node = this;
-            while (node) {
-                if (node.matches(css))
-                    return node;
-                else
-                    node = node.parentElement;
-            }
-            return null;
-        };
-    }
-})();
-(function () {
-    if (!Element.prototype.matches) {
-        Element.prototype.matches = Element.prototype.matchesSelector ||
-                Element.prototype.webkitMatchesSelector ||
-                Element.prototype.mozMatchesSelector ||
-                Element.prototype.msMatchesSelector;
-    }
-})();
