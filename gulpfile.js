8218e2e11a10a4344691cc0a8fe63971318a4272
Alexey <Alexey@Alexey_PC>
2016-12-11 18:42:57 +0200

    промежуточный вариант, не работающий при $ gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
+var mmq = require('gulp-merge-media-queries');
+
+gulp.task('mmq', function () {
+    gulp.src('public/css/*.css')
+            .pipe(mmq())
+            .pipe(gulp.dest('public/css'));
+});
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -9,9 +18,11 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
+
 elixir(function (mix) {
     mix.less('app.less')
             .styles(['flags.css', 'app.css'], 'public/css/all.css', 'public/css')
+            .task('mmq')
             .scripts(['radio_btn.js'], "public/js/radio_btn.js")
             .scripts(['leagues.js'], "public/js/leagues.js")
             .scripts(['all.js'], "public/js/all.js")
